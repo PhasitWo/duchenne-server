efc419bcce8d6bee7f25c84b24a6265ce5afeea1
PhasitWo <100210119+PhasitWo@users.noreply.github.com>
2024-12-12 11:55:55 +0700

    fix cron spec

--- main.go
+++ main.go
@@ -103,8 +103,8 @@ func setupRouter() *gin.Engine {
 
 func InitCronScheduler(db *sql.DB) *cron.Cron {
 	c := cron.New()
-	// everyday on 10.00 (GMT +7) -> spec : "00 03 * * *"
-	c.AddFunc("00 04 * * *", func() {
+	// everyday on 10.00 (GMT +7) -> spec : "00 00 03 * * *"
+	c.AddFunc("00 00 03 * *", func() {
 		mainLogger.Println("Executing Push Notifications..")
 		notification.MockupScheduleNotifications(db, notification.SendRequest)
 	})
